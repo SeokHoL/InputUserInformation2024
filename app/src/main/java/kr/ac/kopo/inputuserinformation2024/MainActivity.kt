8c9db7e50661773b55d641fc20e3dc01a65274ba
SeokHoL <sukho8407@gmail.com>
2024-05-30 15:19:32 +0900

    직접풀어보기 완성

--- app/src/main/java/kr/ac/kopo/inputuserinformation2024/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/inputuserinformation2024/MainActivity.kt
@@ -10,8 +10,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textName : TextView
-    lateinit var textEmail: TextView
+    lateinit var textName : EditText
+    lateinit var textEmail: EditText
     lateinit var btnDlg : Button
     lateinit var dlgEditName : EditText
     lateinit var dlgEditEmail : EditText
@@ -35,13 +35,15 @@ class MainActivity : AppCompatActivity() {
             dlgView = View.inflate(this@MainActivity, R.layout.dialog, null)
             var dialog =AlertDialog.Builder(this@MainActivity)
             dialog.setTitle("사용자 정보 입력")
-            dialog.setIcon(R.drawable.star)
+            dialog.setIcon(R.drawable.soju)
+            dlgEditName = dlgView.findViewById(R.id.editName)
+            dlgEditEmail = dlgView.findViewById(R.id.editEmail)
+            dlgEditName.text=textName.text
+            dlgEditEmail.text=textEmail.text
             dialog.setView(dlgView)
             dialog.setPositiveButton("확인"){dialogL, which->
-                dlgEditName = dlgView.findViewById(R.id.editName)
-                dlgEditEmail = dlgView.findViewById(R.id.editEmail)
-                textName.text =dlgEditName.text.toString()
-                textEmail.text =dlgEditEmail.text.toString()
+                textName.text =dlgEditName.text
+                textEmail.text =dlgEditEmail.text
             }
             dialog.setNegativeButton("취소"){dialogL, which ->
                 toastView =View.inflate(this@MainActivity,R.layout.toast, null)
